119585bc995a685a6279af1e39706c71ab6584ff
cameron fuller <cameronmfuller@yahoo.com>
2018-01-10 17:58:12 -0500

    letter.js

---
+++ Letter.js
@@ -0,0 +1,19 @@
+// Letter constructor
+
+var Letter = function (letter, display) {
+
+    this.letter = letter.toUpperCase();
+    this.display = " _ ";
+
+    // Methods
+
+    // Initially will return underscores
+    this.printLetters = function () {
+        return this.display;
+    }
+
+    // Changes the character to the correct letter
+    this.changeLetter = function() {
+        this.display = letter
+    }
+}
